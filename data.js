d233f16366c70ef1843f630c38b8bfb07c76e341
IceKirby <IceKirby@users.noreply.github.com>
2018-04-27 05:16:08 +0000

    Update data.js

--- data.js
+++ data.js
@@ -190,6 +190,33 @@ var data = [
     {
         type: "Other",
         title: [
+            "Golden Week bonus",
+            "Special login bonus",
+            "Special deals in the Trade Shop"
+        ],
+        image: "https://i.imgur.com/x7WRDbr.png",
+        column: 2,
+        autohide: true,
+        timers: [
+            {
+                name: "Login Bonus",
+                start: "Apr 27 2018, 0:00",
+                end: "May 7 2018, 23:59",
+                info: "Login every day for gems (1st day: 80 gems, 2nd day: 40 gems, 3rd~9th day: 20 gems, 10th day: 40 gems)",
+                autohide: true
+            },
+            {
+                name: "Trade Shop deals",
+                start: "May 1 2018, 0:00",
+                end: "May 9 2018, 23:59",
+                info: "10x Summon Tickets (Price: 200 gems), 10x Golden Clocks (Price: 50 gems). Each can only be bough once.",
+                autohide: true
+            }
+        ]
+    },
+    {
+        type: "Other",
+        title: [
             "Comic Girls Login bonus",
             "Receive 20 gems each day the anime airs",
             "Every thursday until June 21st"
