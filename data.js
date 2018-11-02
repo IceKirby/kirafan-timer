dc929fe38baf2d54121657066bd6dd2ddff1227d
IceKirby <IceKirby@users.noreply.github.com>
2018-11-02 05:41:09 -0300

    Update data.js

--- data.js
+++ data.js
@@ -342,6 +342,45 @@ var timerData = [
         ]
     },
     {
+        type: "WeekendBoss",
+        title: [
+            "November Boss Challenge",
+            "Fight a boss every weekend",
+            "Receive Challenge Medals"
+        ],
+        image: "https://i.imgur.com/v42ZDD7.png",
+        column: 2,
+        timers: [
+            {
+                name: "Weekend 1",
+                start: "November 3 2018, 0:00",
+                end: "November 4 2018, 23:59"
+            },
+            {
+                name: "Weekend 2",
+                start: "November 10 2018, 0:00",
+                end: "November 11 2018, 23:59"
+            },
+            {
+                name: "Weekend 3",
+                start: "November 17 2018, 0:00",
+                end: "November 18 2018, 23:59"
+            },
+            {
+                name: "Weekend 4",
+                start: "November 24 2018, 0:00",
+                end: "November 25 2018, 23:59"
+            },
+            {
+                name: "Medal exchange period",
+                start: "November 3 2018, 0:00",
+                end: "November 30 2018, 23:59",
+                keepAfterFinished: "1 day",
+                type: "normal"
+            }
+        ]
+    },
+    {
         type: "Other",
         title: [
             "Special Gem Sale",
