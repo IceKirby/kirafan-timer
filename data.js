2057765769f3a852a8509e02e33d33362ecb4a57
IceKirby <IceKirby@users.noreply.github.com>
2019-01-28 03:27:51 -0200

    Update data.js

--- data.js
+++ data.js
@@ -17,7 +17,7 @@ var timerData = [
         type: "Maintenance",
         title: [
             "Maintenance",
-            "Version 1.14.0 Update"
+            "Event Preparation"
         ],
         image: "Maintenance",
         column: 0,
@@ -25,8 +25,8 @@ var timerData = [
         timers: [
             {
                 name: "Scheduled Time",
-                start: "January 17 2019, 12:00",
-                end: "January 17 2019, 17:00",
+                start: "January 29 2019, 14:00",
+                end: "January 29 2019, 17:00",
                 keepAfterFinished: "30 minutes"
             }
         ]
