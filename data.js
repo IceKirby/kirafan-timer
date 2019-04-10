ff7fa57b8ae2e9550670ecea88dcbb009a169643
IceKirby <IceKirby@users.noreply.github.com>
2019-04-10 00:42:44 -0300

    Update data.js

--- data.js
+++ data.js
@@ -19,7 +19,7 @@ var timerData = [
             "Maintenance",
             "Summon Renovation",
             "Event Preparation",
-            "Full Voice for Main Quest chapters 3 and 4"
+            "Version 1.16.0 Update"
         ],
         image: "Maintenance",
         column: 0,
@@ -27,8 +27,8 @@ var timerData = [
         timers: [
             {
                 name: "Scheduled Time",
-                start: "March 28 2019, 14:00",
-                end: "March 28 2019, 17:00",
+                start: "April 11 2019, 12:00",
+                end: "April 11 2019, 17:00",
                 keepAfterFinished: "30 minutes"
             }
         ]
