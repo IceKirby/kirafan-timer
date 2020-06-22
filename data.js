41f594a016fef1d74a22b87a6953cce7c1c04b70
IceKirby <IceKirby@users.noreply.github.com>
2020-06-22 00:00:20 -0300

    Update data.js

--- data.js
+++ data.js
@@ -20,7 +20,7 @@ var timerData = [
             "Maintenance",
             "Event Preparation",
             "Summon Renovation",
-            "Version 2.1.1 Update"
+            "Version 2.2.0 Update"
         ],
         image: "Maintenance",
         column: 0,
@@ -28,8 +28,8 @@ var timerData = [
         timers: [
             {
                 name: "Scheduled Time",
-                start: "June 9 2020, 16:00",
-                end: "June 9 2020, 17:00",
+                start: "June 23 2020, 12:00",
+                end: "June 23 2020, 17:00",
                 keepAfterFinished: "30 minutes"
             }
         ]
