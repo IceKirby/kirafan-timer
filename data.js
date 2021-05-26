14a76014df4a976ae64443617d79af464ff53006
IceKirby <IceKirby@users.noreply.github.com>
2021-05-26 05:34:07 -0300

    Update data.js

--- data.js
+++ data.js
@@ -37,73 +37,6 @@ var timerData = [
     {
         "type": "Event",
         "title": [
-            "No Laughing in the Maid Manor!",
-            "メイド館では笑えない！",
-            "Maid 2021 Event"
-        ],
-        "image": [
-            "https://i.imgur.com/NjKzQaN.png",
-            "https://i.imgur.com/5UpNoBs.png",
-            "https://i.imgur.com/XVjhDho.png",
-            "https://i.imgur.com/pLSTVc3.png",
-            "https://i.imgur.com/g8JoM7F.png"
-        ],
-        "column": 0,
-        "priority": 32,
-        "timers": [
-            {
-                "name": "Main Event",
-                "start": "April 27 2021, 17:00",
-                "end": "May 14 2021, 15:59",
-                "markers": [
-                    {
-                        "title": "Extra Chapter 1",
-                        "time": "May 5 2021, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 2",
-                        "time": "May 6 2021, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 3",
-                        "time": "May 7 2021, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 4",
-                        "time": "May 8 2021, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Boss Quest",
-                        "time": "April 29 2021, 17:00",
-                        "type": "battle"
-                    },
-                    {
-                        "title": "EX Quest",
-                        "time": "May 1 2021, 17:00",
-                        "type": "battle"
-                    }
-                ],
-                "extraPriority": 4
-            },
-            {
-                "name": "Missions",
-                "start": "April 27 2021, 17:00",
-                "end": "May 14 2021, 23:59"
-            },
-            {
-                "name": "Event Shop",
-                "start": "April 27 2021, 17:00",
-                "end": "May 21 2021, 13:59"
-            }
-        ]
-    },
-    {
-        "type": "Event",
-        "title": [
             "Giant Kuromon vs Mecha Match",
             "ジャイアントクロモンＶＳメカマッチ",
             "Sansha Sanyou x Sakura Trick x Slow Start Event"
@@ -157,6 +90,73 @@ var timerData = [
         ]
     },
     {
+        "type": "Event",
+        "title": [
+            "Bridal or Hell",
+            "ブライダル・オア・ヘル",
+            "Bride Event"
+        ],
+        "image": [
+            "https://i.imgur.com/z2sEpxB.png",
+            "https://i.imgur.com/b6tUoUR.png",
+            "https://i.imgur.com/H93nXSz.png",
+            "https://i.imgur.com/DNgHfjm.png",
+            "https://i.imgur.com/PWfoSfW.png"
+        ],
+        "column": 0,
+        "priority": 32,
+        "timers": [
+            {
+                "name": "Main Event",
+                "start": "May 27 2021, 17:00",
+                "end": "June 9 2021, 15:59",
+                "extraPriority": 4,
+                "markers": [
+                    {
+                        "title": "EX Quest",
+                        "time": "May 31 2021, 17:00",
+                        "type": "battle"
+                    },
+                    {
+                        "title": "Boss Quest",
+                        "time": "May 29 2021, 17:00",
+                        "type": "battle"
+                    },
+                    {
+                        "title": "Extra Chapter 1",
+                        "time": "June 4 2021, 0:00",
+                        "type": "story"
+                    },
+                    {
+                        "title": "Extra Chapter 2",
+                        "time": "June 5 2021, 0:00",
+                        "type": "story"
+                    },
+                    {
+                        "title": "Extra Chapter 3",
+                        "time": "June 6 2021, 0:00",
+                        "type": "story"
+                    },
+                    {
+                        "title": "Extra Chapter 4",
+                        "time": "June 7 2021, 0:00",
+                        "type": "story"
+                    }
+                ]
+            },
+            {
+                "name": "Missions",
+                "start": "May 27 2021, 17:00",
+                "end": "June 9 2021, 23:59"
+            },
+            {
+                "name": "Event Shop",
+                "start": "May 27 2021, 17:00",
+                "end": "June 16 2021, 13:59"
+            }
+        ]
+    },
+    {
         "type": "Gacha",
         "title": [
             "Giant Kuromon vs Mecha Match Summon",
@@ -185,6 +185,42 @@ var timerData = [
                         "title": "Second Half",
                         "desc": "Featuring 5★ Ikeno Kaede, 4★ Ikeno Kaede, 4★ Iizuka Yuzu",
                         "time": "May 18 2021, 0:00"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "type": "Gacha",
+        "title": [
+            "Bridal or Hell Summon",
+            "ブライダル",
+            "Bride Event Summon"
+        ],
+        "image": [
+            "https://i.imgur.com/z2sEpxB.png",
+            "https://i.imgur.com/b6tUoUR.png",
+            "https://i.imgur.com/H93nXSz.png",
+            "https://i.imgur.com/DNgHfjm.png"
+        ],
+        "column": 1,
+        "priority": 21,
+        "timers": [
+            {
+                "name": "Summon Period",
+                "start": "May 27 2021, 17:00",
+                "end": "June 9 2021, 15:59",
+                "extraPriority": 6,
+                "banners": [
+                    {
+                        "title": "Part 1",
+                        "desc": "Featuring 5★ Kon [Bridal], 5★ Kohane [Bridal]",
+                        "time": "May 27 2021, 17:00"
+                    },
+                    {
+                        "title": "Part 2",
+                        "desc": "Featuring 5★ Ruki [Bridal], 5★ Sesame [Bridal]",
+                        "time": "May 27 2021, 17:00"
                     }
                 ]
             }
@@ -345,26 +381,44 @@ var timerData = [
     {
         "type": "Gacha",
         "title": [
-            "Head Priest & Seven Sages Random Summon Ticket",
-            "筆頭神官＆七賢者限定★5確定チケット付き！有償限定10回召喚",
-            "400 Paid Gems for a 10x Summon + Random 5* Summon Ticket"
+            "Kirara Fantasia Limited-time Summon",
+            "きららファンタジア期間限定復刻ピックアップ召喚"
         ],
         "image": [
-            "clea"
+            "https://i.imgur.com/EwMOT1D.png"
         ],
         "column": 1,
         "priority": 21,
         "timers": [
             {
-                "name": "Summon Period",
-                "start": "April 27 2021, 0:00",
-                "end": "May 4 2021, 23:59",
-                "extraPriority": 3,
+                "name": "Summon Period: Featuring 5★ Kirara [Manga ver.]",
+                "start": "May 27 2021, 17:00",
+                "end": "May 31 2021, 23:59",
+                "extraPriority": 4
+            }
+        ]
+    },
+    {
+        "type": "Gacha",
+        "title": [
+            "Head Priest & Seven Sages Random Summon Ticket",
+            "筆頭神官＆七賢者限定★5確定チケット付き！有償限定10回召喚",
+            "400 Paid Gems for a 10x Summon + Random 5★ Summon Ticket"
+        ],
+        "image": "clea",
+        "column": 1,
+        "priority": 21,
+        "timers": [
+            {
+                "name": "Summon Period: Featuring 5★ Archive, 5★ Sugar, 5★ Sesame, 5★ Cardamom, 5★ Salt, 5★ Ginger, 5★ Fennel, 5★ Hakka",
+                "start": "May 27 2021, 17:00",
+                "end": "June 1 2021, 23:59",
+                "extraPriority": 4
             },
             {
                 "name": "Ticket Expiration",
-                "start": "April 27 2021, 0:00",
-                "end": "May 11 2021, 23:59"
+                "start": "May 27 2021, 17:00",
+                "end": "June 8 2021, 23:59"
             }
         ]
     },
@@ -380,15 +434,22 @@ var timerData = [
         "priority": 21,
         "timers": [
             {
-                "name": "Part 1: Featuring 5* Shiina [Valentine], 5* Sonya [Halloween], 5* Rin [Traveler of the Wasteland], 5* Rin [White Day], 5* Archive, 5* Archive [New Year]",
-                "start": "May 8 2021, 0:00",
-                "end": "May 14 2021, 15:59",
-                "extraPriority": 4
-            },
-            {
-                "name": "Part 2: Featuring 5* Yuzuko [Swimsuit], 5* Yukari [Swimsuit], 5* Tamaki [Sports Festival], 5* Youko [New Year], 5* Futaba [Swimsuit], 4* Tamaki [Christmas]",
-                "start": "May 8 2021, 0:00",
-                "end": "May 14 2021, 15:59"
+                "name": "Summon Period",
+                "start": "May 28 2021, 0:00",
+                "end": "June 3 2021, 23:59",
+                "extraPriority": 4,
+                "banners": [
+                    {
+                        "title": "Part 1",
+                        "desc": "Featuring 5★ Chiya [Christmas], 5★ Koume [Valentine], 5★ Koyume [Swimsuit], 5★ Tsubasa [Tanabata], 5★ Kotetsu [Christmas], 5★ Sesame",
+                        "time": "May 28 2021, 0:00"
+                    },
+                    {
+                        "title": "Part 2",
+                        "desc": "Featuring 5★ Kon [Swimsuit], 5★ Omi [New Year], 5★ Kaos [Swimsuit], 5★ Hizume [Maid], 5★ Kana [White Day], 5★ Clea [Doll's Festival], 4★ Nono [Halloween]",
+                        "time": "May 28 2021, 0:00"
+                    }
+                ]
             }
         ]
     },
@@ -499,6 +560,45 @@ var timerData = [
         ]
     },
     {
+        "type": "WeekendBoss",
+        "title": [
+            "June Boss Challenge",
+            "Fight a boss every weekend",
+            "Receive Challenge Medals"
+        ],
+        "image": "https://i.imgur.com/nfrALgB.png",
+        "column": 2,
+        "timers": [
+            {
+                "name": "Weekend 1",
+                "start": "June 5 2021, 0:00",
+                "end": "June 6 2021, 23:59"
+            },
+            {
+                "name": "Weekend 2",
+                "start": "June 12 2021, 0:00",
+                "end": "June 13 2021, 23:59"
+            },
+            {
+                "name": "Weekend 3",
+                "start": "June 19 2021, 0:00",
+                "end": "June 20 2021, 23:59"
+            },
+            {
+                "name": "Weekend 4",
+                "start": "June 26 2021, 0:00",
+                "end": "June 27 2021, 23:59"
+            },
+            {
+                "name": "Medal exchange period",
+                "start": "June 5 2021, 0:00",
+                "end": "July 2 2021, 23:59",
+                "keepAfterFinished": "2 hours",
+                "type": "normal"
+            }
+        ]
+    },
+    {
         type: "Other",
         title: [
             "Limited Past Sports Festival Room Decorations",
@@ -517,16 +617,21 @@ var timerData = [
     {
         type: "Other",
         title: [
-            "Skills Update Special Sale",
-            "運動会アイテムショップに期間限定アイテム追加"
+            "Event Celebration Gem Set Sale",
+            "イベント開催記念特別セール！星彩石セット販売"
         ],
         image: "cork",
         column: 2,
         timers: [
             {
-                name: "Item Sets",
-                "start": "May 14 2021, 17:00",
-                "end": "May 20 2021, 16:59"
+                name: "Gem Sets",
+                "start": "May 27 2021, 17:00",
+                "end": "June 9 2021, 16:59"
+            },
+            {
+                name: "Ticket Expiration",
+                "start": "May 27 2021, 17:00",
+                "end": "June 16 2021, 23:59"
             }
         ]
     }
