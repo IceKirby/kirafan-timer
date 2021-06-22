764531032fe9bfb073d292bca84ce3730650c9ff
IceKirby <IceKirby@users.noreply.github.com>
2021-06-22 06:56:24 -0300

    Update data.js

--- data.js
+++ data.js
@@ -21,7 +21,7 @@ var timerData = [
             "Maintenance",
             "Event Preparation",
             "Summon Renovation",
-            "Version 3.1.2 Update"
+            "Version 3.2.0 Update"
         ],
         image: "Maintenance",
         column: 0,
@@ -29,76 +29,9 @@ var timerData = [
         timers: [
             {
                 name: "Scheduled Time",
-                start: "June 9 2021, 16:00",
-                end: "June 9 2021, 17:00",
+                start: "June 24 2021, 12:00",
+                end: "June 24 2021, 17:00",
                 keepAfterFinished: "30 minutes"
-            }
-        ]
-    },
-    {
-        "type": "Event",
-        "title": [
-            "Bridal or Hell",
-            "ブライダル・オア・ヘル",
-            "Bride Event"
-        ],
-        "image": [
-            "https://i.imgur.com/z2sEpxB.png",
-            "https://i.imgur.com/b6tUoUR.png",
-            "https://i.imgur.com/H93nXSz.png",
-            "https://i.imgur.com/DNgHfjm.png",
-            "https://i.imgur.com/PWfoSfW.png"
-        ],
-        "column": 0,
-        "priority": 32,
-        "timers": [
-            {
-                "name": "Main Event",
-                "start": "May 27 2021, 17:00",
-                "end": "June 9 2021, 15:59",
-                "extraPriority": 4,
-                "markers": [
-                    {
-                        "title": "EX Quest",
-                        "time": "May 31 2021, 17:00",
-                        "type": "battle"
-                    },
-                    {
-                        "title": "Boss Quest",
-                        "time": "May 29 2021, 17:00",
-                        "type": "battle"
-                    },
-                    {
-                        "title": "Extra Chapter 1",
-                        "time": "June 4 2021, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 2",
-                        "time": "June 5 2021, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 3",
-                        "time": "June 6 2021, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 4",
-                        "time": "June 7 2021, 0:00",
-                        "type": "story"
-                    }
-                ]
-            },
-            {
-                "name": "Missions",
-                "start": "May 27 2021, 17:00",
-                "end": "June 9 2021, 23:59"
-            },
-            {
-                "name": "Event Shop",
-                "start": "May 27 2021, 17:00",
-                "end": "June 16 2021, 13:59"
             }
         ]
     },
@@ -172,38 +105,38 @@ var timerData = [
         ]
     },
     {
-        "type": "Gacha",
+        "type": "Event",
         "title": [
-            "Bridal or Hell Summon",
-            "ブライダル",
-            "Bride Event Summon"
+            "Let's Train and Challenge! Ability Tree Implementation Quest",
+            "育てて挑もう！アビリティツリー実装記念クエスト",
+            "Ability Tree Mini-Event"
         ],
-        "image": [
-            "https://i.imgur.com/z2sEpxB.png",
-            "https://i.imgur.com/b6tUoUR.png",
-            "https://i.imgur.com/H93nXSz.png",
-            "https://i.imgur.com/DNgHfjm.png"
-        ],
-        "column": 1,
-        "priority": 21,
+        "image": ["kirara"],
+        "column": 0,
+        "priority": 32,
         "timers": [
             {
-                "name": "Summon Period",
-                "start": "May 27 2021, 17:00",
-                "end": "June 9 2021, 15:59",
-                "extraPriority": 6,
-                "banners": [
+                "name": "Main Event",
+                "start": "June 24 2021, 17:00",
+                "end": "July 13 2021, 23:59",
+                "extraPriority": 4,
+                "markers": [
                     {
-                        "title": "Part 1",
-                        "desc": "Featuring 5★ Kon [Bridal], 5★ Kohane [Bridal]",
-                        "time": "May 27 2021, 17:00"
-                    },
-                    {
-                        "title": "Part 2",
-                        "desc": "Featuring 5★ Ruki [Bridal], 5★ Sesame [Bridal]",
-                        "time": "May 27 2021, 17:00"
+                        "title": "Farming Quest Part 2 + Super Hard Quest",
+                        "time": "July 2 2021, 0:00",
+                        "type": "battle"
                     }
                 ]
+            },
+            {
+                "name": "Missions",
+                "start": "June 24 2021, 17:00",
+                "end": "July 13 2021, 23:59"
+            },
+            {
+                "name": "Event Shop",
+                "start": "June 24 2021, 17:00",
+                "end": "July 20 2021, 13:59"
             }
         ]
     },
@@ -277,55 +210,6 @@ var timerData = [
     {
         "type": "Gacha",
         "title": [
-            "Limited-time Alchemist Step-up Summon / Paid Summon",
-            "2つの召喚を同時開催！アルケミストのキャラクターを仲間にしよう！",
-            "Random 5★ Summon Ticket on the 7th step",
-            "240 Paid Gems for a 10x Summon + Random 5★ Summon Ticket"
-        ],
-        "image": "clea",
-        "column": 1,
-        "priority": 21,
-        "timers": [
-            {
-                "name": "Summon Period: Featuring 5★ Nagi, 5★ Aya [Valentine], 5★ Miki [Halloween], 5★ Koume [Valentine], 5★ Nadeshiko [Valentine], 5★ Kuro, 5★ Konohata Mira, 5★ Kawaguchi Yoshino",
-                "start": "June 4 2021, 17:00",
-                "end": "June 9 2021, 15:59",
-                "extraPriority": 4
-            },
-            {
-                "name": "Ticket Expiration",
-                "start": "June 4 2021, 17:00",
-                "end": "June 16 2021, 23:59"
-            }
-        ]
-    },
-    {
-        "type": "Gacha",
-        "title": [
-            "Head Priest & Seven Sages Random Summon Ticket",
-            "筆頭神官＆七賢者限定★5確定チケット付き！有償限定10回召喚",
-            "400 Paid Gems for a 10x Summon + Random 5★ Summon Ticket"
-        ],
-        "image": "clea",
-        "column": 1,
-        "priority": 21,
-        "timers": [
-            {
-                "name": "Summon Period: Featuring 5★ Archive, 5★ Sugar, 5★ Sesame, 5★ Cardamom, 5★ Salt, 5★ Ginger, 5★ Fennel, 5★ Hakka",
-                "start": "May 27 2021, 17:00",
-                "end": "June 1 2021, 23:59",
-                "extraPriority": 4
-            },
-            {
-                "name": "Ticket Expiration",
-                "start": "May 27 2021, 17:00",
-                "end": "June 8 2021, 23:59"
-            }
-        ]
-    },
-    {
-        "type": "Gacha",
-        "title": [
             "Daily KiraKira Limited-time Summon",
             "1日1回期間限定きらきら召喚",
             "40 Paid Gems once per day for 3 summons, all 4* or above"
@@ -339,6 +223,108 @@ var timerData = [
                 "start": "June 17 2021, 0:00",
                 "end": "June 24 2021, 11:59",
                 "extraPriority": 4
+            }
+        ]
+    },
+    {
+        "type": "Gacha",
+        "title": [
+            "Ability Tree Celebration Daily KiraKira Limited-time Summon",
+            "アビリティツリー実装記念1日1回期間限定きらきら召喚・特装版",
+            "40 Paid Gems once per day for 3 summons, all 4* or above",
+            "Collect Ticket Scraps and exchange for a Random 5* Summon Ticket"
+        ],
+        "image": "clea",
+        "column": 1,
+        "priority": 21,
+        "timers": [
+            {
+                "name": "Summon Period: Featuring Limited-Time 5★ Characters released in 2020",
+                "start": "June 24 2021, 17:00",
+                "end": "July 1 2021, 23:59",
+                "extraPriority": 4
+            },
+            {
+                "name": "Ticket Exchange",
+                "start": "June 24 2021, 17:00",
+                "end": "July 1 2021, 23:59"
+            },
+            {
+                "name": "Ticket Expiration",
+                "start": "June 24 2021, 17:00",
+                "end": "July 8 2021, 23:59"
+            }
+        ]
+    },
+    {
+        "type": "Gacha",
+        "title": [
+            "2020 Limited-time Step-up Summon",
+            "アビリティツリー実装記念期間限定2020復刻おまけ付き10回召喚",
+            "Random 5★ Summon Ticket on the 7th step"
+        ],
+        "image": "clea",
+        "column": 1,
+        "priority": 21,
+        "timers": [
+            {
+                "name": "Summon Period: Featuring Limited-Time 5★ Characters released in 2020",
+                "start": "June 24 2021, 17:00",
+                "end": "July 1 2021, 23:59",
+                "extraPriority": 4
+            },
+            {
+                "name": "Ticket Expiration",
+                "start": "June 24 2021, 17:00",
+                "end": "July 8 2021, 23:59"
+            }
+        ]
+    },
+    {
+        "type": "Gacha",
+        "title": [
+            "Ability Tree Celebration Choosable 5* Summon Ticket",
+            "アビリティツリー実装記念★5選べるチケット付き！有償限定10回召喚",
+            "400 Paid Gems for a 10x Summon + Choosable 5★ Summon Ticket"
+        ],
+        "image": "clea",
+        "column": 1,
+        "priority": 21,
+        "timers": [
+            {
+                "name": "Summon Period",
+                "start": "June 24 2021, 17:00",
+                "end": "July 1 2021, 23:59",
+                "extraPriority": 4
+            },
+            {
+                "name": "Ticket Expiration",
+                "start": "June 24 2021, 17:00",
+                "end": "July 22 2021, 23:59"
+            }
+        ]
+    },
+    {
+        "type": "Gacha",
+        "title": [
+            "Ability Tree Celebration Past Limited-Time 5* Paid Summon",
+            "アビリティツリー実装記念　期間限定復刻★5選べるチケット付き！有償限定10回召喚",
+            "1,000 Paid Gems for a 10x Summon + 2x Choosable 5★ Summon Ticket"
+        ],
+        "image": "clea",
+        "column": 1,
+        "priority": 21,
+        "timers": [
+            {
+                "name": "Summon Period: Featuring Limited-Time 5★ Characters released between 2017~2019",
+                "start": "June 24 2021, 17:00",
+                "end": "June 29 2021, 23:59",
+                "extraPriority": 4
+            },
+            {
+                "name": "Ticket Expiration",
+                "start": "June 24 2021, 17:00",
+                "end": "July 6 2021, 23:59"
             }
         ]
     },
@@ -494,70 +480,6 @@ var timerData = [
         ]
     },
     {
-        type: "Other",
-        title: [
-            "Event Celebration Gem Set Sale",
-            "イベント開催記念特別セール！星彩石セット販売"
-        ],
-        image: "cork",
-        column: 2,
-        timers: [
-            {
-                name: "Gem Sets",
-                "start": "May 27 2021, 17:00",
-                "end": "June 9 2021, 16:59"
-            },
-            {
-                name: "Ticket Expiration",
-                "start": "May 27 2021, 17:00",
-                "end": "June 16 2021, 23:59"
-            }
-        ]
-    },
-    {
-        "type": "Gacha",
-        "title": [
-            "Manga Volume 2 Celebration Daily KiraKira Limited-time Summon",
-            "コミカライズ第2巻発売記念1日1回期間限定きらきら召喚",
-            "40 Paid Gems once per day for 3 summons, all 4* or above"
-        ],
-        "image": "clea",
-        "column": 1,
-        "priority": 21,
-        "timers": [
-            {
-                "name": "Summon Period: Featuring 5★ Yuno [Christmas], 5★ Yuno [Swimsuit], 5★ Miyako [Christmas], 5★ Yasuna [New Year], 5★ Cocoa [Christmas], 5★ Cocoa [Swimsuit]",
-                "start": "June 1 2021, 0:00",
-                "end": "June 9 2021, 15:59",
-                "extraPriority": 4
-            }
-        ]
-    },
-    {
-        "type": "Gacha",
-        "title": [
-            "Manga Volume 2 Celebration Paid Summon + Random 5* Summon Ticket",
-            "コミカライズ第2巻発売記念★5確定チケット付き！有償限定10回召喚",
-            "240 Paid Gems for a 10x Summon + Random 5★ Summon Ticket"
-        ],
-        "image": "clea",
-        "column": 1,
-        "priority": 21,
-        "timers": [
-            {
-                "name": "Summon Period: Featuring characters that appear in the Volume 2",
-                "start": "June 1 2021, 0:00",
-                "end": "June 9 2021, 15:59",
-                "extraPriority": 4
-            },
-            {
-                "name": "Ticket Expiration",
-                "start": "June 1 2021, 0:00",
-                "end": "June 16 2021, 23:59"
-            }
-        ]
-    },
-    {
         "type": "Other",
         "title": [
             "The Girls from a Close yet Distant World Login Bonus",
@@ -652,6 +574,73 @@ var timerData = [
                 "name": "Period",
                 "start": "June 9 2021, 17:00",
                 "end": "June 24 2021, 11:59"
+            }
+        ]
+    },
+    {
+        "type": "Other",
+        "title": [
+            "Ver 3.2.0 Celebration Login Bonus",
+            "Ver 3.2.0記念ログインボーナス",
+            "Log in on 7 days to receive up to 4x Quest Key - Tree + 15x Stamina Recovery Item - Large."
+        ],
+        "image": "lamp",
+        "column": 2,
+        "priority": 15,
+        "timers": [
+            {
+                "name": "Period",
+                "start": "June 24 2021, 17:00",
+                "end": "July 13 2021, 23:59"
+            }
+        ]
+    },
+    {
+        "type": "Other",
+        "title": [
+            "Ver 3.2.0 Celebration Campaign Sales",
+            "Ver 3.2.0記念セール"
+        ],
+        "image": "cork",
+        "column": 2,
+        "priority": 12,
+        "timers": [
+            {
+                "name": "Ability Tree-related Items",
+                "start": "June 24 2021, 17:00",
+                "end": "July 13 2021, 23:59"
+            },
+            {
+                "name": "Gem Set",
+                "start": "June 24 2021, 17:00",
+                "end": "July 13 2021, 23:59"
+            },
+            {
+                "name": "Item Sets",
+                "start": "June 24 2021, 17:00",
+                "end": "July 13 2021, 23:59"
+            },
+            {
+                "name": "Limited-time 2020 Random 5★ Summon Ticket, Star Festival 5★ Support Set",
+                "start": "June 24 2021, 17:00",
+                "end": "July 1 2021, 23:59"
+            }
+        ]
+    },
+    {
+        "type": "Mission",
+        "title": [
+            "Ver 3.2.0 Celebration Missions",
+            "Ver 3.2.0記念ミッション"
+        ],
+        "image": "match",
+        "column": 0,
+        "priority": 30,
+        "timers": [
+            {
+                "name": "Period",
+                "start": "June 24 2021, 17:00",
+                "end": "July 13 2021, 23:59"
             }
         ]
     }
