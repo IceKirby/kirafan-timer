2fca8f13e83d58027f39865a619331c83bd96923
IceKirby <IceKirby@users.noreply.github.com>
2021-09-22 05:15:41 -0300

    Update data.js

--- data.js
+++ data.js
@@ -300,6 +300,25 @@ var timerData = [
     {
         "type": "Gacha",
         "title": [
+            "Manga Time Kirara Carat Limited-time Deluxe Summon",
+            "まんがタイムきららキャラット期間限定デラックス召喚",
+            "1000 Paid Gems for a 10x 5* Summon"
+        ],
+        "image": "clea",
+        "column": 1,
+        "priority": 21,
+        "timers": [
+            {
+                "name": "Summon Period",
+                "start": "September 24 2021, 0:00",
+                "end": "September 25 2021, 23:59",
+                "extraPriority": 4
+            }
+        ]
+    },
+    {
+        "type": "Gacha",
+        "title": [
             "Daily KiraKira Limited-time Summon",
             "1日1回期間限定きらきら召喚",
             "40 Paid Gems once per day for 3 summons, all 4* or above"
