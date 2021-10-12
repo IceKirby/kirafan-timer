94182acbe80cde05ed6eaff89d1366bc0d178ad7
IceKirby <IceKirby@users.noreply.github.com>
2021-10-12 04:54:03 -0300

    Update data.js

--- data.js
+++ data.js
@@ -28,8 +28,8 @@ var timerData = [
         timers: [
             {
                 name: "Scheduled Time",
-                start: "September 29 2021, 16:00",
-                end: "September 29 2021, 17:00",
+                start: "October 13 2021, 16:00",
+                end: "October 13 2021, 17:00",
                 keepAfterFinished: "30 minutes"
             }
         ]
