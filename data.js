fe53d554d54ee262a8cf95cebf1f68d3fe7e8812
IceKirby <IceKirby@users.noreply.github.com>
2022-07-26 06:08:27 -0300

    Update data.js

--- data.js
+++ data.js
@@ -38,67 +38,68 @@ var timerData = [
     {
         "type": "Event",
         "title": [
-            "Tanabata's Parting Star",
-            "七夕のわかれ星",
-            "Tanabata 2022 Event"
+            "Midsummer's Etowaria Fes",
+            "真夏のエトワリアフェス",
+            "Swimsuit 2022 Event"
         ],
         "image": [
-            "https://i.imgur.com/QWVV6C0.png",
-            "https://i.imgur.com/KCtcSql.png",
-            "https://i.imgur.com/qBZ3vMx.png",
-            "https://i.imgur.com/UmxCNwm.png",
-            "https://i.imgur.com/LDnMVby.png"
+            "https://i.imgur.com/VoZSUoW.png",
+            "https://i.imgur.com/pesnGNF.png",
+            "https://i.imgur.com/Zb0gGYI.png",
+            "https://i.imgur.com/fDBfrCn.png",
+            "https://i.imgur.com/XBMQ6Bf.png",
+            "https://i.imgur.com/IRZLDwg.png"
         ],
         "column": 0,
         "priority": 32,
         "timers": [
             {
                 "name": "Main Event",
-                "start": "June 28 2022, 17:00",
-                "end": "July 12 2022, 15:59",
+                "start": "July 27 2022, 17:00",
+                "end": "August 25 2022, 14:59",
                 "extraPriority": 4,
                 "markers": [
                     {
                         "title": "EX Quest",
-                        "time": "July 2 2022, 17:00",
+                        "time": "August 3 2022, 17:00",
                         "type": "battle"
                     },
                     {
                         "title": "Boss Quest",
-                        "time": "June 30 2022, 17:00",
+                        "time": "July 29 2022, 17:00",
                         "type": "battle"
                     },
                     {
                         "title": "Extra Chapter 1",
-                        "time": "July 7 2022, 0:00",
+                        "time": "August 6 2022, 0:00",
                         "type": "story"
                     },
                     {
                         "title": "Extra Chapter 2",
-                        "time": "July 8 2022, 0:00",
+                        "time": "August 7 2022, 0:00",
                         "type": "story"
                     },
                     {
                         "title": "Extra Chapter 3",
-                        "time": "July 9 2022, 0:00",
+                        "time": "August 8 2022, 0:00",
                         "type": "story"
                     },
                     {
                         "title": "Extra Chapter 4",
-                        "time": "July 10 2022, 0:00",
+                        "time": "August 9 2022, 0:00",
                         "type": "story"
                     }
                 ]
             },
             {
                 "name": "Missions",
-                "start": "June 28 2022, 17:00",
-                "end": "July 12 2022, 23:59"
+                "start": "July 27 2022, 17:00",
+                "end": "August 25 2022, 23:59"
             },
             {
                 "name": "Event Shop",
-                "start": "June 28 2022, 17:00",
-                "end": "July 19 2022, 13:59"
+                "start": "July 27 2022, 17:00",
+                "end": "September 1 2022, 13:59"
             }
         ]
     },
@@ -232,32 +233,45 @@ var timerData = [
     {
         "type": "Gacha",
         "title": [
-            "Main Quest Part 2 Limited-time Paid Summon + Random 5★ Summon Ticket",
-            "きららファンタジア第2部限定★5確定チケット付き！有償限定10回召喚",
-            "400 Paid Gems for a 10x Summon + Random 5★ Summon Ticket"
+            "Midsummer's Etowaria Fes Summon",
+            "期間限定2022水着キャラクターピックアップ召喚",
+            "Swimsuit 2022 Event Summon"
         ],
-        "image": "clea",
+        "image": [
+            "https://i.imgur.com/VoZSUoW.png",
+            "https://i.imgur.com/pesnGNF.png",
+            "https://i.imgur.com/Zb0gGYI.png",
+            "https://i.imgur.com/fDBfrCn.png",
+            "https://i.imgur.com/XBMQ6Bf.png"
+        ],
         "column": 1,
         "priority": 21,
         "timers": [
             {
                 "name": "Summon Period",
-                "start": "July 5 2022, 0:00",
-                "end": "July 12 2022, 15:59",
-                "extraPriority": 4
-            },
-            {
-                "name": "Ticket Expiration",
-                "start": "July 5 2022, 0:00",
-                "end": "July 19 2022, 23:59"
+                "start": "July 27 2022, 17:00",
+                "end": "August 25 2022, 14:59",
+                "banners": [
+                    {
+                        "title": "Part 1",
+                        "desc": "Featuring 5★ Hifumi [Swimsuit], 5★ Shiina [Swimsuit], 5★ Koharu [Swimsuit]",
+                        "time": "July 27 2022, 17:00"
+                    },
+                    {
+                        "title": "Part 2",
+                        "desc": "Featuring 5★ Hayu [Swimsuit], 5★ Fa [Swimsuit]",
+                        "time": "July 27 2022, 17:00"
+                    }
+                ],
+                "extraPriority": 6
             }
         ]
     },
     {
         "type": "Gacha",
         "title": [
-            "Main Quest Part 2 Limited-time Step-up Summon",
-            "きららファンタジア第2部限定復刻おまけ付き10回召喚",
+            "Limited-time Past Swimsuit Step-up Summon",
+            "期間限定水着復刻おまけ付き10回召喚",
             "Random 5★ Summon Ticket on the 7th step"
         ],
         "image": "clea",
@@ -265,39 +279,69 @@ var timerData = [
         "priority": 21,
         "timers": [
             {
-                "name": "Summon Period",
-                "start": "July 5 2022, 0:00",
-                "end": "July 12 2022, 15:59",
+                "name": "Summon Period: Featuring past Swimsuit characters",
+                "start": "July 27 2022, 17:00",
+                "end": "August 3 2022, 23:59",
                 "extraPriority": 4
             },
             {
                 "name": "Ticket Expiration",
-                "start": "July 5 2022, 0:00",
-                "end": "July 19 2022, 23:59"
+                "start": "July 27 2022, 17:00",
+                "end": "August 10 2022, 23:59"
             }
         ]
     },
     {
         "type": "Gacha",
         "title": [
-            "Kirara's Birthday Celebration Paid Summon + Choosable 5★ Summon Ticket",
-            "きららお誕生日記念★5選べるチケット付き！有償限定10回召喚",
-            "1,000 Paid Gems for a 10x Summon + 5x Choosable 5★ Summon Ticket + 1x Kirara's Birthday User Title Medal"
+            "Past Limited-time Swimsuit Paid Summon + Choosable 5★ Summon Ticket",
+            "期間限定水着復刻★5選べるチケット付き！有償限定10回召喚",
+            "1000 Paid Gems for a 10x Summon + 2x Choosable 5★ Summon Ticket"
         ],
         "image": "clea",
         "column": 1,
         "priority": 21,
         "timers": [
             {
-                "name": "Summon Period: Featuring 5★ Kirara [Manga ver.], 5★ Kirara [Cross Chara]",
-                "start": "July 7 2022, 0:00",
-                "end": "July 9 2022, 23:59",
+                "name": "First Half",
+                "start": "July 27 2022, 17:00",
+                "end": "August 3 2022, 23:59",
+                "extraPriority": 4
+            },
+            {
+                "name": "Second Half",
+                "start": "August 4 2022, 0:00",
+                "end": "August 10 2022, 15:59",
                 "extraPriority": 4
             },
             {
                 "name": "Ticket Expiration",
-                "start": "July 7 2022, 0:00",
-                "end": "July 16 2022, 23:59"
+                "start": "July 27 2022, 17:00",
+                "end": "August 17 2022, 23:59"
+            }
+        ]
+    },
+    {
+        "type": "Gacha",
+        "title": [
+            "All Stars Paid Summon + Choosable 5★ Summon Ticket",
+            "オールスター★5選べるチケット付き！有償限定10回召喚",
+            "700 Paid Gems for a 10x Summon + 2x Choosable 5★ Summon Ticket"
+        ],
+        "image": "clea",
+        "column": 1,
+        "priority": 21,
+        "timers": [
+            {
+                "name": "Summon Period",
+                "start": "July 27 2022, 17:00",
+                "end": "August 2 2022, 23:59",
+                "extraPriority": 4
+            },
+            {
+                "name": "Ticket Expiration",
+                "start": "July 27 2022, 17:00",
+                "end": "August 9 2022, 23:59"
             }
         ]
     },
@@ -446,6 +490,30 @@ var timerData = [
         ]
     },
     {
+        "type": "Gacha",
+        "title": [
+            "Daily KiraKira Limited-time Summon",
+            "1日1回期間限定きらきら召喚",
+            "40 Paid Gems once per day for 3 summons, all 4* or above"
+        ],
+        "image": "clea",
+        "column": 1,
+        "priority": 21,
+        "timers": [
+            {
+                "name": "Summon Period: Featuring past Swimsuit characters",
+                "start": "July 27 2022, 17:00",
+                "end": "August 3 2022, 23:59",
+                "extraPriority": 4
+            },
+            {
+                "name": "Ticket Expiration",
+                "start": "July 27 2022, 17:00",
+                "end": "August 10 2022, 23:59"
+            }
+        ]
+    },
+    {
         type: "LoginDays",
         title: [
             "RPG Fudousan participation Login Bonus",
@@ -482,6 +550,11 @@ var timerData = [
                 name: "Part 1 (Chapters 1~Epilogue)",
                 "start": "July 12 2022, 17:00",
                 "end": "July 27 2022, 15:59"
+            },
+            {
+                "name": "Part 1 (Chapters 1~Epilogue, Normal Mode) and Part 2 (Chapters 1~7)",
+                "start": "July 27 2022, 17:00",
+                "end": "August 10 2022, 15:59"
             }
         ]
     },
@@ -551,19 +624,70 @@ var timerData = [
                 "end": "July 27 2022, 15:59"
             },
             {
-                "name": "Summer Memory 5* Summon Set A & B",
-                "start": "July 16 2022, 0:00",
-                "end": "July 19 2022, 23:59"
-            },
-            {
-                "name": "Summer Memory 5* Summon Set C",
-                "start": "July 20 2022, 0:00",
-                "end": "July 23 2022, 23:59"
-            },
-            {
                 "name": "Summer Memory 5* Summon Set D",
                 "start": "July 24 2022, 0:00",
                 "end": "July 27 2022, 15:59"
+            },
+            
+            {
+                "name": "Summer Vacation Campaign Item & Starlight Stone Sets A",
+                "start": "July 27 2022, 17:00",
+                "end": "August 25 2022, 14:59"
+            },
+            {
+                "name": "Summer Vacation Campaign Item & Starlight Stone Sets B",
+                "start": "July 27 2022, 17:00",
+                "end": "August 10 2022, 15:59"
+            },
+            {
+                "name": "Summer Vacation Campaign Item & Starlight Stone Sets C",
+                "start": "July 27 2022, 17:00",
+                "end": "August 2 2022, 23:59"
+            },
+            {
+                "name": "Random Past Swimsuit 5★ Summon Ticket Sets",
+                "start": "July 27 2022, 17:00",
+                "end": "August 3 2022, 23:59"
+            },
+            {
+                "name": "Summer Vacation 2022 Set",
+                "start": "July 27 2022, 17:00",
+                "end": "August 10 2022, 15:59"
+            }
+        ]
+    },
+    {
+        "type": "Other",
+        "title": [
+            "Etowaria's Summer Vacation 2022 First Half Login Bonus",
+            "2022エトワリアの夏休み 前半 ログインボーナス",
+            "Log in on 10 days to receive up to 200 Starlight Stones."
+        ],
+        "image": "lamp",
+        "column": 2,
+        "priority": 15,
+        "timers": [
+            {
+                "name": "Period",
+                "start": "July 27 2022, 17:00",
+                "end": "August 10 2022, 23:59"
+            }
+        ]
+    },
+    {
+        "type": "Other",
+        "title": [
+            "Summer-themed Room Decoration Items",
+            "夏限定ルームアイテム再登場"
+        ],
+        "image": "kanna",
+        "column": 2,
+        "priority": 10,
+        "timers": [
+            {
+                "name": "Period",
+                "start": "July 27 2022, 17:00",
+                "end": "August 25 2022, 14:59"
             }
         ]
     }
