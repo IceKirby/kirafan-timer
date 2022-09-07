ebc8c489c1d380e69c16a1a9e682ccaa5c9d27b4
IceKirby <IceKirby@users.noreply.github.com>
2022-09-07 05:14:46 -0300

    Update data.js

--- data.js
+++ data.js
@@ -37,147 +37,6 @@ var timerData = [
     {
         "type": "Event",
         "title": [
-            "Beach Shark Panic",
-            "ビーチシャークパニック",
-            "Swimsuit 2022 Event 2"
-        ],
-        "image": [
-            "https://i.imgur.com/vM8YbCb.png",
-            "https://i.imgur.com/WOBBp7N.png",
-            "https://i.imgur.com/hjSF34s.png",
-            "https://i.imgur.com/H7kQbXZ.png",
-            "https://i.imgur.com/RD2shlE.png",
-            "https://i.imgur.com/GuM2lt7.png"
-        ],
-        "column": 0,
-        "priority": 32,
-        "timers": [
-            {
-                "name": "Main Event",
-                "start": "August 10 2022, 17:00",
-                "end": "August 25 2022, 14:59",
-                "extraPriority": 4,
-                "markers": [
-                    {
-                        "title": "EX Quest",
-                        "time": "August 14 2022, 17:00",
-                        "type": "battle"
-                    },
-                    {
-                        "title": "Boss Quest",
-                        "time": "August 12 2022, 17:00",
-                        "type": "battle"
-                    },
-                    {
-                        "title": "Super EX Quest",
-                        "time": "August 17 2022, 17:00",
-                        "type": "battle"
-                    },
-                    {
-                        "title": "Extra Chapter 1",
-                        "time": "August 19 2022, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 2",
-                        "time": "August 20 2022, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 3",
-                        "time": "August 21 2022, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 4",
-                        "time": "August 22 2022, 0:00",
-                        "type": "story"
-                    }
-                ]
-            },
-            {
-                "name": "Missions",
-                "start": "August 10 2022, 17:00",
-                "end": "August 25 2022, 23:59"
-            },
-            {
-                "name": "Event Shop",
-                "start": "August 10 2022, 17:00",
-                "end": "September 1 2022, 13:59"
-            }
-        ]
-    },
-    {
-        "type": "Event",
-        "title": [
-            "Midsummer's Etowaria Fes",
-            "真夏のエトワリアフェス",
-            "Swimsuit 2022 Event"
-        ],
-        "image": [
-            "https://i.imgur.com/VoZSUoW.png",
-            "https://i.imgur.com/pesnGNF.png",
-            "https://i.imgur.com/Zb0gGYI.png",
-            "https://i.imgur.com/fDBfrCn.png",
-            "https://i.imgur.com/XBMQ6Bf.png",
-            "https://i.imgur.com/IRZLDwg.png"
-        ],
-        "column": 0,
-        "priority": 32,
-        "timers": [
-            {
-                "name": "Main Event",
-                "start": "July 27 2022, 17:00",
-                "end": "August 25 2022, 14:59",
-                "extraPriority": 4,
-                "markers": [
-                    {
-                        "title": "EX Quest",
-                        "time": "August 3 2022, 17:00",
-                        "type": "battle"
-                    },
-                    {
-                        "title": "Boss Quest",
-                        "time": "July 29 2022, 17:00",
-                        "type": "battle"
-                    },
-                    {
-                        "title": "Extra Chapter 1",
-                        "time": "August 6 2022, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 2",
-                        "time": "August 7 2022, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 3",
-                        "time": "August 8 2022, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 4",
-                        "time": "August 9 2022, 0:00",
-                        "type": "story"
-                    }
-                ]
-            },
-            {
-                "name": "Missions",
-                "start": "July 27 2022, 17:00",
-                "end": "August 25 2022, 23:59"
-            },
-            {
-                "name": "Event Shop",
-                "start": "July 27 2022, 17:00",
-                "end": "September 1 2022, 13:59"
-            }
-        ]
-    },
-    {
-        "type": "Event",
-        "title": [
             "[Rerun] Kurumi in Wonderland",
             "復刻・くるみインワンダーランド",
             "Rerun of Kurumi 2018 Event"
@@ -273,6 +132,70 @@ var timerData = [
         ]
     },
     {
+        "type": "Event",
+        "title": [
+            "Opening! Etowaria Academy's Cultural Festival",
+            "開催！エトワリア学園文化祭",
+            "Power of Smile. & Koharu Biyori. Event"
+        ],
+        "image": [
+            "https://i.imgur.com/C8ShvcS.png",
+            "https://i.imgur.com/wkEvXSI.png"
+        ],
+        "column": 0,
+        "priority": 32,
+        "timers": [
+            {
+                "name": "Main Event",
+                "start": "September 8 2022, 17:00",
+                "end": "September 27 2022, 15:59",
+                "extraPriority": 4,
+                "markers": [
+                    {
+                        "title": "EX Quest",
+                        "time": "September 12 2022, 17:00",
+                        "type": "battle"
+                    },
+                    {
+                        "title": "Boss Quest",
+                        "time": "September 10 2022, 17:00",
+                        "type": "battle"
+                    },
+                    {
+                        "title": "Extra Chapter 1",
+                        "time": "September 21 2022, 0:00",
+                        "type": "story"
+                    },
+                    {
+                        "title": "Extra Chapter 2",
+                        "time": "September 22 2022, 0:00",
+                        "type": "story"
+                    },
+                    {
+                        "title": "Extra Chapter 3",
+                        "time": "September 23 2022, 0:00",
+                        "type": "story"
+                    },
+                    {
+                        "title": "Extra Chapter 4",
+                        "time": "September 24 2022, 0:00",
+                        "type": "story"
+                    }
+                ]
+            },
+            {
+                "name": "Missions",
+                "start": "September 8 2022, 17:00",
+                "end": "September 27 2022, 23:59"
+            },
+            {
+                "name": "Event Shop",
+                "start": "September 8 2022, 17:00",
+                "end": "October 4 2022, 13:59"
+            }
+        ]
+    },
+    {
         "type": "Other",
         "title": [
             "KiraFan Summer Memory Campaign",
@@ -358,94 +281,62 @@ var timerData = [
     {
         "type": "Gacha",
         "title": [
-            "Super EX Quest Support Paid Summon + Choosable 5★ Summon Ticket",
-            "超高難易度クエスト応援★5選べるチケット付き！有償限定10回召喚",
-            "400 Paid Gems for a 10x Summon + Choosable 5★ Summon Ticket + Super EX Quest Voucher"
+            "Opening! Etowaria Academy's Cultural Festival Summon",
+            "開催！エトワリア学園文化祭ピックアップ召喚",
+            "Power of Smile. & Koharu Biyori. Event Summon"
         ],
-        "image": "clea",
+        "image": [
+            "https://i.imgur.com/C8ShvcS.png",
+            "https://i.imgur.com/wkEvXSI.png"
+        ],
         "column": 1,
         "priority": 21,
         "timers": [
             {
-                "name": "First Half",
-                "start": "August 14 2022, 0:00",
-                "end": "August 19 2022, 23:59",
-                "extraPriority": 4
-            },
-            {
-                "name": "Second Half",
-                "start": "August 20 2022, 0:00",
-                "end": "August 25 2022, 14:59",
-                "extraPriority": 4
-            },
-            {
-                "name": "Ticket Expiration",
-                "start": "August 14 2022, 0:00",
-                "end": "September 1 2022, 23:59"
+                "name": "Summon Period",
+                "start": "September 8 2022, 17:00",
+                "end": "September 27 2022, 15:59",
+                "banners": [
+                    {
+                        "title": "Nina Side",
+                        "desc": "Featuring 5★ Tachibana Nina, 4★ Tachibana Nina",
+                        "time": "September 8 2022, 17:00"
+                    },
+                    {
+                        "title": "Tamaki Side",
+                        "desc": "Featuring 5★ Kodou Tamaki, 4★ Kodou Tamaki",
+                        "time": "September 8 2022, 17:00"
+                    }
+                ],
+                "extraPriority": 6
             }
         ]
     },
     {
         "type": "Gacha",
         "title": [
-            "Paid Summon + Choosable 5★ Summon Ticket",
-            "★5選べるチケット付き！有償限定10回召喚",
-            "400 Paid Gems for a 10x Summon + 2x Choosable 5★ Summon Ticket + 1x Summer Vacation Voucher"
+            "Cultural Festival-themed Step-up + Paid Summon",
+            "2つの召喚を同時開催！文化祭を楽しんでいる生徒を仲間にしよう！",
+            "600 Paid Gems for a 10x Summon + 4x Random 5★ Summon Ticket",
+            "Random 5★ Summon Ticket on the 7th step"
         ],
         "image": "clea",
         "column": 1,
         "priority": 21,
         "timers": [
             {
-                "name": "First Half",
-                "start": "August 10 2022, 17:00",
-                "end": "August 17 2022, 23:59",
-                "extraPriority": 4
-            },
-            {
-                "name": "Second Half",
-                "start": "August 18 2022, 0:00",
-                "end": "August 25 2022, 14:59",
+                "name": "Summon Period: Featuring characters from the current event",
+                "start": "September 9 2022, 0:00",
+                "end": "September 16 2022, 23:59",
                 "extraPriority": 4
             },
             {
                 "name": "Ticket Expiration",
-                "start": "August 10 2022, 17:00",
-                "end": "September 1 2022, 23:59"
+                "start": "September 9 2022, 0:00",
+                "end": "September 23 2022, 23:59"
             }
         ]
     },
-    {
-        "type": "Gacha",
-        "title": [
-            "Limited-time Paid Summon + Choosable 5★ Summon Ticket",
-            "期間限定★5選べるチケット付き！有償限定10回召喚",
-            "1,000 Paid Gems for a 10x Summon + 2x Choosable 5★ Summon Ticket + 1x Extra Call Voucher"
-        ],
-        "image": "clea",
-        "column": 1,
-        "priority": 21,
-        "timers": [
-            {
-                "name": "First Half",
-                "start": "August 10 2022, 17:00",
-                "end": "August 17 2022, 23:59",
-                "extraPriority": 4
-            },
-            {
-                "name": "Second Half",
-                "start": "August 18 2022, 0:00",
-                "end": "August 25 2022, 14:59",
-                "extraPriority": 4
-            },
-            {
-                "name": "Ticket Expiration",
-                "start": "August 10 2022, 17:00",
-                "end": "September 1 2022, 23:59"
-            }
-        ]
-    },
-    
     {
         "type": "Gacha",
         "title": [
@@ -507,25 +398,6 @@ var timerData = [
         ]
     },
     {
-        "type": "Gacha",
-        "title": [
-            "Daily KiraKira Limited-time Summon",
-            "1日1回期間限定きらきら召喚",
-            "40 Paid Gems once per day for 3 summons, all 4* or above"
-        ],
-        "image": "clea",
-        "column": 1,
-        "priority": 21,
-        "timers": [
-            {
-                "name": "Summon Period: Featuring characters from the Anne Happy♪ rerun events",
-                "start": "August 25 2022, 17:00",
-                "end": "August 31 2022, 23:59",
-                "extraPriority": 4
-            }
-        ]
-    },
-    {
         type: "LoginDays",
         title: [
             "RPG Fudousan participation Login Bonus",
@@ -567,46 +439,6 @@ var timerData = [
                 "name": "Part 1 (Chapters 1~Epilogue, Normal Mode) and Part 2 (Chapters 1~7)",
                 "start": "July 27 2022, 17:00",
                 "end": "August 10 2022, 15:59"
-            }
-        ]
-    },
-    {
-        "type": "WeekendBoss",
-        "title": [
-            "August Boss Challenge",
-            "Fight a boss every weekend",
-            "Receive Challenge Medals"
-        ],
-        "image": "https://i.imgur.com/uXUtgKZ.png",
-        "column": 2,
-        "priority": 13,
-        "timers": [
-            {
-                "name": "Weekend 1",
-                "start": "August 6 2022, 0:00",
-                "end": "August 7 2022, 23:59"
-            },
-            {
-                "name": "Weekend 2",
-                "start": "August 13 2022, 0:00",
-                "end": "August 14 2022, 23:59"
-            },
-            {
-                "name": "Weekend 3",
-                "start": "August 20 2022, 0:00",
-                "end": "August 21 2022, 23:59"
-            },
-            {
-                "name": "Weekend 4",
-                "start": "August 27 2022, 0:00",
-                "end": "August 28 2022, 23:59"
-            },
-            {
-                "name": "Medal exchange period",
-                "start": "August 6 2022, 0:00",
-                "end": "September 2 2022, 23:59",
-                "keepAfterFinished": "2 hours",
-                "type": "normal"
             }
         ]
     },
@@ -661,11 +493,6 @@ var timerData = [
         "priority": 12,
         "timers": [
             {
-                "name": "Anne Happy♪ Rerun Campaign Item & Starlight Stone Sets",
-                "start": "August 25 2022, 17:00",
-                "end": "September 1 2022, 23:59"
-            },
-            {
                 "name": "KiraFan Summer Memory Post-Festival Campaign Item & Starlight Stone Sets",
                 "start": "August 25 2022, 17:00",
                 "end": "September 8 2022, 15:59"
@@ -674,57 +501,6 @@ var timerData = [
                 "name": "Manga Volume 4 Release Starlight Stone Bundle",
                 "start": "September 1 2022, 0:00",
                 "end": "September 8 2022, 15:59"
-            }
-        ]
-    },
-    {
-        "type": "Other",
-        "title": [
-            "Training Support Campaign",
-            "育成応援キャンペーン"
-        ],
-        "image": "leine",
-        "column": 0,
-        "priority": 30,
-        "timers": [
-            {
-                "name": "Quests Period",
-                "start": "August 14th 2022, 0:00",
-                "end": "August 25th 2022, 14:59",
-                "extraPriority": 2
-            },
-            {
-                "name": "Event Missions",
-                "start": "August 14th 2022, 0:00",
-                "end": "August 25th 2022, 23:59"
-            },
-            {
-                "name": "Event Shop",
-                "start": "August 14th 2022, 0:00",
-                "end": "September 1st 2022, 13:59"
-            }
-        ]
-    },
-    {
-        "type": "Gacha",
-        "title": [
-            "1700 Days Celebration Paid Summon + Choosable 5★ Summon Ticket",
-            "リリース1700日記念★5選べるチケット付き！有償限定10回召喚",
-            "400 Paid Stones for a 10x Summon + 2x Choosable 5★ Summon Ticket + Summer Vacation Voucher"
-        ],
-        "image": "clea",
-        "column": 1,
-        "priority": 21,
-        "timers": [
-            {
-                "name": "Period",
-                "start": "August 6 2022, 0:00",
-                "end": "August 13 2022, 23:59"
-            },
-            {
-                "name": "Ticket Expiration",
-                "start": "August 6 2022, 0:00",
-                "end": "September 1 2022, 23:59"
             }
         ]
     },
