8ad6635cd7ef1ea14967c8d4ca62eca3006ca1d3
IceKirby <IceKirby@users.noreply.github.com>
2022-09-26 05:25:07 -0300

    Update data.js

--- data.js
+++ data.js
@@ -38,103 +38,6 @@ var timerData = [
     {
         "type": "Event",
         "title": [
-            "[Rerun] Kurumi in Wonderland",
-            "復刻・くるみインワンダーランド",
-            "Rerun of Kurumi 2018 Event"
-        ],
-        "image": ["https://i.imgur.com/wgnhxt2.png"],
-        "column": 0,
-        "priority": 32,
-        "timers": [
-            {
-                "name": "Main Event",
-                "start": "August 25 2022, 17:00",
-                "end": "September 8 2022, 15:59",
-                "extraPriority": 4,
-                "markers": [
-                    {
-                        "title": "Melee Quest",
-                        "time": "August 27 2022, 17:00",
-                        "type": "battle"
-                    },
-                    {
-                        "title": "Extra Chapter 1",
-                        "time": "September 1 2022, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 2",
-                        "time": "September 2 2022, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 3",
-                        "time": "September 3 2022, 0:00",
-                        "type": "story"
-                    },
-                    {
-                        "title": "Extra Chapter 4",
-                        "time": "September 4 2022, 0:00",
-                        "type": "story"
-                    }
-                ]
-            },
-            {
-                "name": "Missions",
-                "start": "August 25 2022, 17:00",
-                "end": "September 8 2022, 23:59"
-            },
-            {
-                "name": "Event Shop",
-                "start": "August 25 2022, 17:00",
-                "end": "September 15 2022, 13:59"
-            }
-        ]
-    },
-    {
-        "type": "Event",
-        "title": [
-            "[Rerun] Hibari, Rii-san and the Flower of Happiness",
-            "復刻・ヒバリとりーさんと幸福の花",
-            "Rerun of Hibari/Rii-san 2018 Event"
-        ],
-        "image": ["https://i.imgur.com/wgnhxt2.png"],
-        "column": 0,
-        "priority": 32,
-        "timers": [
-            {
-                "name": "Main Event",
-                "start": "August 25 2022, 17:00",
-                "end": "September 8 2022, 15:59",
-                "extraPriority": 4,
-                "markers": [
-                    {
-                        "title": "EX Quest",
-                        "time": "August 29 2022, 17:00",
-                        "type": "battle"
-                    },
-                    {
-                        "title": "Boss Quest",
-                        "time": "August 27 2022, 17:00",
-                        "type": "battle"
-                    }
-                ]
-            },
-            {
-                "name": "Missions",
-                "start": "August 25 2022, 17:00",
-                "end": "September 8 2022, 23:59"
-            },
-            {
-                "name": "Event Shop",
-                "start": "August 25 2022, 17:00",
-                "end": "September 15 2022, 13:59"
-            }
-        ]
-    },
-    {
-        "type": "Event",
-        "title": [
             "Opening! Etowaria Academy's Cultural Festival",
             "開催！エトワリア学園文化祭",
             "Power of Smile. & Koharu Biyori. Event"
@@ -199,6 +102,73 @@ var timerData = [
     {
         "type": "Event",
         "title": [
+            "Great Escape! Hot Springs Prison",
+            "大脱出！温泉プリズン",
+            "Hot Springs (Autumn) 2022 Event"
+        ],
+        "image": [
+            "https://i.imgur.com/N7IAVG0.png",
+            "https://i.imgur.com/NZLo3Ee.png",
+            "https://i.imgur.com/InwimDO.png",
+            "https://i.imgur.com/6pXO0mo.png",
+            "https://i.imgur.com/qtmxNBr.png"
+        ],
+        "column": 0,
+        "priority": 32,
+        "timers": [
+            {
+                "name": "Main Event",
+                "start": "September 27 2022, 17:00",
+                "end": "October 12 2022, 15:59",
+                "extraPriority": 4,
+                "markers": [
+                    {
+                        "title": "EX Quest",
+                        "time": "October 1 2022, 17:00",
+                        "type": "battle"
+                    },
+                    {
+                        "title": "Boss Quest",
+                        "time": "September 29 2022, 17:00",
+                        "type": "battle"
+                    },
+                    {
+                        "title": "Extra Chapter 1",
+                        "time": "October 6 2022, 0:00",
+                        "type": "story"
+                    },
+                    {
+                        "title": "Extra Chapter 2",
+                        "time": "October 7 2022, 0:00",
+                        "type": "story"
+                    },
+                    {
+                        "title": "Extra Chapter 3",
+                        "time": "October 8 2022, 0:00",
+                        "type": "story"
+                    },
+                    {
+                        "title": "Extra Chapter 4",
+                        "time": "October 9 2022, 0:00",
+                        "type": "story"
+                    }
+                ]
+            },
+            {
+                "name": "Missions",
+                "start": "September 27 2022, 17:00",
+                "end": "October 12 2022, 23:59"
+            },
+            {
+                "name": "Event Shop",
+                "start": "September 27 2022, 17:00",
+                "end": "October 19 2022, 13:59"
+            }
+        ]
+    },
+    {
+        "type": "Event",
+        "title": [
             "Treasure Hoarding Campaign",
             "お宝ザクザクキャンペーン"
         ],
@@ -216,71 +186,6 @@ var timerData = [
                 "name": "Event Shop / Mystery Treasure Chest",
                 "start": "September 17 2022, 0:00",
                 "end": "October 4 2022, 13:59"
-            }
-        ]
-    },
-    {
-        "type": "Other",
-        "title": [
-            "KiraFan Summer Memory Campaign",
-            "きらファンサマーメモリーキャンペーン"
-        ],
-        "image": "kirara",
-        "column": 0,
-        "priority": 30,
-        "timers": [
-            {
-                "name": "Daily Missions",
-                "start": "July 16 2022, 0:00",
-                "end": "September 9 2022, 23:59",
-                "extraPriority": 2
-            },
-            {
-                "name": "Summer Memory Quest",
-                "start": "July 16 2022, 0:00",
-                "end": "August 3 2022, 23:59",
-                "markers": [
-                    {
-                        "title": "Otohime",
-                        "time": "July 16 2022, 0:00",
-                        "type": "battle"
-                    },
-                    {
-                        "title": "Locco & Lotte",
-                        "time": "July 20 2022, 0:00",
-                        "type": "battle"
-                    },
-                    {
-                        "title": "Angalga & Summer Sweets Golem",
-                        "time": "July 24 2022, 0:00",
-                        "type": "battle"
-                    },
-                    {
-                        "title": "Pirate's Henchman & Nowhere's Pierrot",
-                        "time": "July 27 2022, 17:00",
-                        "type": "battle"
-                    }
-                ]
-            },
-            {
-                "name": "Ticket Expiration",
-                "start": "July 16 2022, 0:00",
-                "end": "September 16 2022, 23:59"
-            },
-            {
-                "name": "Event Shop",
-                "start": "July 16 2022, 0:00",
-                "end": "August 10 2022, 13:59"
-            },
-            {
-                "name": "Twitter Campaign Retweets Goal + Extra Reward",
-                "start": "July 27 2022, 17:00",
-                "end": "July 28 2022, 23:59"
-            },
-            {
-                "name": "Twitter Campaign Likes Goal Reward",
-                "start": "August 10 2022, 17:00",
-                "end": "August 11 2022, 23:59"
             }
         ]
     },
@@ -341,55 +246,36 @@ var timerData = [
     {
         "type": "Gacha",
         "title": [
-            "Cultural Festival-themed Step-up + Paid Summon",
-            "2つの召喚を同時開催！文化祭を楽しんでいる生徒を仲間にしよう！",
-            "600 Paid Gems for a 10x Summon + 4x Random 5★ Summon Ticket",
-            "Random 5★ Summon Ticket on the 7th step"
+            "Great Escape! Hot Springs Prison Summon",
+            "大脱出！温泉プリズン期間限定ピックアップ召喚",
+            "Hot Springs (Autumn) 2022 Event Summon"
         ],
-        "image": "clea",
+        "image": [
+            "https://i.imgur.com/N7IAVG0.png",
+            "https://i.imgur.com/NZLo3Ee.png",
+            "https://i.imgur.com/InwimDO.png",
+            "https://i.imgur.com/6pXO0mo.png"
+        ],
         "column": 1,
         "priority": 21,
         "timers": [
             {
-                "name": "Summon Period: Featuring characters from the current event",
-                "start": "September 9 2022, 0:00",
-                "end": "September 16 2022, 23:59",
-                "extraPriority": 4
-            },
-            {
-                "name": "Ticket Expiration",
-                "start": "September 9 2022, 0:00",
-                "end": "September 23 2022, 23:59"
-            }
-        ]
-    },
-    {
-        "type": "Gacha",
-        "title": [
-            "All-Stars 2022 Deluxe Summon",
-            "2022オールスターデラックス召喚",
-            "1000 Paid Gems for a 10x 5* Summon"
-        ],
-        "image": "clea",
-        "column": 1,
-        "priority": 21,
-        "timers": [
-            {
-                "name": "Summon Period: Featuring all characters released in 2022 (Until July 12th)",
-                "start": "August 25 2022, 17:00",
-                "end": "September 1 2022, 23:59",
-                "extraPriority": 4
-            },
-            {
-                "name": "Summon Period (Encore): Featuring all characters released in 2022 (Until July 12th)",
-                "start": "September 2 2022, 0:00",
-                "end": "September 8 2022, 15:59",
-                "extraPriority": 4
-            },
-            {
-                "name": "Ticket Expiration",
-                "start": "August 25 2022, 17:00",
-                "end": "September 15 2022, 23:59"
+                "name": "Summon Period",
+                "start": "September 27 2022, 17:00",
+                "end": "October 12 2022, 15:59",
+                "banners": [
+                    {
+                        "title": "Part 1",
+                        "desc": "Featuring 5★ Chiho [Hot Springs], 5★ Youko [Hot Springs]",
+                        "time": "September 27 2022, 17:00"
+                    },
+                    {
+                        "title": "Part 2",
+                        "desc": "Featuring 5★ Nono [Hot Springs], 5★ Monroe [Hot Springs]",
+                        "time": "September 27 2022, 17:00"
+                    }
+                ],
+                "extraPriority": 6
             }
         ]
     },
@@ -445,6 +331,60 @@ var timerData = [
                 "name": "Ticket Expiration",
                 "start": "September 17 2022, 0:00",
                 "end": "October 4 2022, 23:59"
+            }
+        ]
+    },
+    {
+        "type": "Gacha",
+        "title": [
+            "Past Hot Spring Limited-time Step-up Summon",
+            "期間限定温泉復刻おまけ付き10回召喚",
+            "Random 5★ Summon Ticket on the 7th step"
+        ],
+        "image": "clea",
+        "column": 1,
+        "priority": 21,
+        "timers": [
+            {
+                "name": "Summon Period: Featuring 5★ Yuuri [Hot Springs], 5★ Hiroe [Hot Springs], 5★ Fuura [Hot Springs], 5★ Hibari [Hot Springs], 5★ Haruka [Hot Springs], 5★ Kana [Hot Springs], 5★ Leine [Hot Springs], 5★ Archive [Hot Springs]",
+                "start": "September 27 2022, 17:00",
+                "end": "October 2 2022, 23:59",
+                "extraPriority": 4
+            },
+            {
+                "name": "Ticket Expiration",
+                "start": "September 27 2022, 17:00",
+                "end": "October 9 2022, 23:59"
+            }
+        ]
+    },
+    {
+        "type": "Gacha",
+        "title": [
+            "Past Hot Spring Limited-time Paid Summon + Choosable 5★ Summon Ticket",
+            "期間限定温泉復刻★5選べるチケット付き！有償限定10回召喚",
+            "1000 Paid Gems for a 10x Summon + 2x Choosable 5★ Summon Ticket + 1x Hot Spring Voucher"
+        ],
+        "image": "clea",
+        "column": 1,
+        "priority": 21,
+        "timers": [
+            {
+                "name": "First Half: Featuring 5★ Yuuri [Hot Springs], 5★ Hiroe [Hot Springs], 5★ Fuura [Hot Springs], 5★ Hibari [Hot Springs], 5★ Haruka [Hot Springs], 5★ Kana [Hot Springs], 5★ Leine [Hot Springs], 5★ Archive [Hot Springs]",
+                "start": "September 27 2022, 17:00",
+                "end": "October 2 2022, 23:59",
+                "extraPriority": 4
+            },
+            {
+                "name": "Second Half: Featuring 5★ Yuuri [Hot Springs], 5★ Hiroe [Hot Springs], 5★ Fuura [Hot Springs], 5★ Hibari [Hot Springs], 5★ Haruka [Hot Springs], 5★ Kana [Hot Springs], 5★ Leine [Hot Springs], 5★ Archive [Hot Springs]",
+                "start": "October 3 2022, 0:00",
+                "end": "October 7 2022, 23:59",
+                "extraPriority": 4
+            },
+            {
+                "name": "Ticket Expiration",
+                "start": "September 27 2022, 17:00",
+                "end": "October 14 2022, 23:59"
             }
         ]
     },
@@ -577,30 +517,26 @@ var timerData = [
                 "name": "Silver Week Campaign Item & Starlight Stone Sets",
                 "start": "September 17 2022, 0:00",
                 "end": "September 27 2022, 15:59"
-            }
-        ]
-    },
-    {
-        "type": "Gacha",
-        "title": [
-            "Manga Volume 4 Release Paid Summon + Random 5★ Summon Ticket",
-            "コミカライズ第4巻発売記念★5確定チケット付き！有償限定10回召喚",
-            "240 Paid Gems for a 10x Summon + Random 5★ Summon Ticket"
-        ],
-        "image": "clea",
-        "column": 1,
-        "priority": 21,
-        "timers": [
-            {
-                "name": "Summon Period: Featuring characters that appear on Volume 4.",
-                "start": "September 1 2022, 0:00",
-                "end": "September 8 2022, 15:59",
-                "extraPriority": 4
             },
             {
-                "name": "Ticket Expiration",
-                "start": "September 1 2022, 0:00",
-                "end": "September 15 2022, 23:59"
+                "name": "Past Hot Spring Random 5★ Summon Ticket Sets",
+                "start": "September 27 2022, 17:00",
+                "end": "October 2 2022, 23:59"
+            },
+            {
+                "name": "Hot Spring 2022 (Autumn) Item & Starlight Stone Sets",
+                "start": "September 27 2022, 17:00",
+                "end": "October 12 2022, 15:59"
+            },
+            {
+                "name": "Kiniro Mosaic Voucher Sets",
+                "start": "September 27 2022, 17:00",
+                "end": "October 4 2022, 23:59"
+            },
+            {
+                "name": "Urara Meirochou Voucher Sets",
+                "start": "October 5 2022, 0:00",
+                "end": "October 12 2022, 15:59"
             }
         ]
     },
